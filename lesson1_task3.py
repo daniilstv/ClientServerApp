7844edfad55e92f7040588705cd0df84d4cd70dc
daniilstv <daniilstv@gmail.com>
2019-12-25 15:57:29 +0300

    lesson1

--- lesson1_task3.py
+++ lesson1_task3.py
@@ -13,5 +13,5 @@ for i in WORDS:
     print(f"Слово - {i}: тип данных {type(i)} , длина {len(i)}, размер {sys.getsizeof(i)} байт")
     print(f"Представление слова в байтах {i.encode('utf-8')}")
 
-    if i is i.encode('utf-8'):
+    if i == i.encode('utf-8'):
         print("Ок")
