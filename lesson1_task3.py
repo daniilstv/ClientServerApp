aa4e612b6fc8807c3dee107edcd412d2ff217023
daniilstv <daniilstv@gmail.com>
2019-12-26 21:46:55 +0300

    lesson1 task5 - final

--- lesson1_task3.py
+++ lesson1_task3.py
@@ -11,7 +11,23 @@ WORDS = ('attribute', 'класс', 'функция', 'type')
 for i in WORDS:
 
     print(f"Слово - {i}: тип данных {type(i)} , длина {len(i)}, размер {sys.getsizeof(i)} байт")
-    print(f"Представление слова в байтах {i.encode('utf-8')}")
+    print(f"Представление слова {i} в байтах {i.encode('utf-8')}")
 
+# Неудачная попытка проверни на идентичность слова и байтовой строки
     if i == i.encode('utf-8'):
-        print("Ок")
+        print("Ок\n")
+
+print("\nВариант по итогу разбора ДЗ\n")
+for i in WORDS:
+    try:
+        print(f'Представление слова "{i}" в байтах {bytes(i, "ascii")}')
+    except UnicodeEncodeError:
+        print(f'Слово "{i}" невозможно представить байтовой строкой')
+
+print("\nВариант 2 по итогу разбора ДЗ\n")
+for i in WORDS:
+    try:
+        a = f"b'{i}'"
+        print(f"Представление слова '{i}' в байтах {exec(a)}")
+    except SyntaxError:
+        print(f'Слово "{i}" невозможно представить байтовой строкой')
