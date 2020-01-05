cabcf31382bc93f72807b1fb50454e484a16a5a5
daniilstv <daniilstv@gmail.com>
2020-01-06 00:58:02 +0300

    add task 2

--- lesson2_task2.py
+++ lesson2_task2.py
@@ -1,10 +1,47 @@
-import unittest
+'''
+2. Задание на закрепление знаний по модулю json. Есть файл orders в формате JSON
+с информацией о заказах. Написать скрипт, автоматизирующий его заполнение данными.
+Для этого:
+Создать функцию write_order_to_json(), в которую передается 5 параметров
+— товар (item), количество (quantity), цена (price), покупатель (buyer),
+дата (date). Функция должна предусматривать запись данных в виде словаря
+в файл orders.json. При записи данных указать величину отступа в 4 пробельных
+символа;
+Проверить работу программы через вызов функции write_order_to_json()
+с передачей в нее значений каждого параметра.
+'''
 
+import json
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+def write_order_to_json(item,quantity,price,buyer,date):
 
+    tmp_dict = {
+        'item': item,
+        'quantity': quantity,
+        'price': price,
+        'buyer': buyer,
+        'date': date
+    }
 
-if __name__ == '__main__':
-    unittest.main()
+    with open('orders_tmp.json', 'w', encoding='utf8') as f_n:
+        f_n.write(json.dumps(tmp_dict, ensure_ascii=False, indent=4))
+
+TEST_DATA = {
+        'item': 'Подстаканник',
+        'quantity': 10,
+        'price': 1500,
+        'buyer': 'Петров',
+        'date': '1.02.2020'
+            }
+
+# print(type(TEST_DATA),TEST_DATA)
+# print(TEST_DATA.get('item'))
+
+write_order_to_json(TEST_DATA.pop('item'),
+                    TEST_DATA.pop('quantity'),
+                    TEST_DATA.pop('price'),
+                    TEST_DATA.pop('buyer'),
+                    TEST_DATA.pop('date'))
+
+with open('orders_tmp.json') as f_n:
+    print(f_n.read())
