7553f2ca84f53a501ebae59654e2767941f61f37
daniilstv <daniilstv@gmail.com>
2020-01-06 01:03:41 +0300

    fix pylint recomendation

--- lesson2_task2.py
+++ lesson2_task2.py
@@ -13,8 +13,10 @@
 
 import json
 
-def write_order_to_json(item,quantity,price,buyer,date):
-
+def write_order_to_json(item, quantity, price, buyer, date):
+    '''
+    Пишет словарь в файл
+    '''
     tmp_dict = {
         'item': item,
         'quantity': quantity,
@@ -23,8 +25,8 @@ def write_order_to_json(item,quantity,price,buyer,date):
         'date': date
     }
 
-    with open('orders_tmp.json', 'w', encoding='utf8') as f_n:
-        f_n.write(json.dumps(tmp_dict, ensure_ascii=False, indent=4))
+    with open('orders_tmp.json', 'w', encoding='utf8') as f_w:
+        f_w.write(json.dumps(tmp_dict, ensure_ascii=False, indent=4))
 
 TEST_DATA = {
         'item': 'Подстаканник',
@@ -32,9 +34,9 @@ TEST_DATA = {
         'price': 1500,
         'buyer': 'Петров',
         'date': '1.02.2020'
-            }
+    }
 
-# print(type(TEST_DATA),TEST_DATA)
+# print(type(TEST_DATA), TEST_DATA)
 # print(TEST_DATA.get('item'))
 
 write_order_to_json(TEST_DATA.pop('item'),
